fd0c7823c0ff909b5f6c5fca410ec20c8885606d
SirBungala <amalgregory123@gmail.com>
2025-02-08 00:33:35 +0530

    Minor UI changes - navbar, logo and overlap of navbar over content

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "../styles/navbar.css";
+import logo from "../assets/logo_transparent.png";
 
 function Navbar() {
   return (
     <nav className="navbar">
+      <div className="navbar-brand">
+        <img src={logo} alt="Logo" className="navbar-logo" />
+      </div>
       <NavLink to="/" className="nav-link">
         Home
       </NavLink>
